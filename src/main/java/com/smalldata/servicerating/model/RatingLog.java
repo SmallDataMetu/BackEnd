8db94d8bccb0d37f9ef6414e8b3ed0e41198b96a
Ramazan Zor <ramazanzor19@gmail.com>
2018-12-02 02:32:00 +0300

    end time of travels added to get-rating-logs api response

--- src/main/java/com/smalldata/servicerating/model/RatingLog.java
+++ src/main/java/com/smalldata/servicerating/model/RatingLog.java
@@ -1,8 +1,12 @@
 package com.smalldata.servicerating.model;
 
+import java.util.Date;
+
 public class RatingLog {
 
     private String travelId;
+
+    private Date endTime;
 
     private EmotionScore rating;
 
@@ -20,5 +24,13 @@ public class RatingLog {
 
     public void setRating(EmotionScore rating) {
         this.rating = rating;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
     }
 }
