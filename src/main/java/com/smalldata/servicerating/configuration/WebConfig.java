f34f48cdc77dadea80f43d1ba3162c305b5f90ba
Ramazan Zor <ramazanzor19@gmail.com>
2018-12-02 09:34:11 +0300

    cors policy update

--- src/main/java/com/smalldata/servicerating/configuration/WebConfig.java
+++ src/main/java/com/smalldata/servicerating/configuration/WebConfig.java
@@ -2,14 +2,18 @@ package com.smalldata.servicerating.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+                .allowCredentials(true);
     }
+
 }
