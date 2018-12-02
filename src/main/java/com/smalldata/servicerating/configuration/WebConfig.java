c03978c5ef53cdbd107757d9698bd226cfcc9296
Ramazan Zor <ramazanzor19@gmail.com>
2018-12-02 10:43:04 +0300

    add options to cors allowed methods

--- src/main/java/com/smalldata/servicerating/configuration/WebConfig.java
+++ src/main/java/com/smalldata/servicerating/configuration/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD");
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS");
     }
 
 }
